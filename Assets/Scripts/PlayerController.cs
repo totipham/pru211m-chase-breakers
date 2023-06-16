c867603e3e3646aa78a9133fcc427ef6545776cb
David Pham <ducptm68@gmail.com>
2023-06-16 23:32:14 +0700

    feature: add joystick

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,93 +5,85 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private SwipeControlManager swipeControlManager;
-
     public float gravity;
+    public float gravityScale = 1.0f;
     public Vector2 velocity;
     public float jumpVelocity = 30f;
+    public float jumpVelocityScale = 1.0f;
     public float groundHeight = -2f;
     public bool isGrounded = false;
     public bool isFall = false;
+    public float acceleration = 10f;
+    public float maxAcceleration = 10f;
+    public float maxVelocity = 100f;
+    public Joystick joystick;
 
-    // Start is called before the first frame update
+    public float initialJumpHeight;
+    public float maxJumpHeight = 5.0f;
+
+// Start is called before the first frame update
     void Start()
     {
-        swipeControlManager = GetComponent<SwipeControlManager>();
+        joystick = GetComponent<Joystick>();
     }
 
-    // Update is called once per frame
+// Update is called once per frame
     void Update()
     {
         Vector2 pos = transform.position;
         if (isGrounded)
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow)) 
-            // if (swipeControlManager.IsSwipingUp())
+            if (Input.GetKeyDown(KeyCode.UpArrow) || joystick.direction.y > 0)
             {
                 isGrounded = false;
-                velocity.y = jumpVelocity;
+
+                // Increase the jump velocity based on the x velocity
+                jumpVelocityScale = 1.0f + Mathf.Pow(velocity.x / maxVelocity, 4);
+
+                velocity.y = jumpVelocity * jumpVelocityScale;
+
+                // Store the initial jump height
+                initialJumpHeight = pos.y;
             }
 
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            if (Input.GetKeyDown(KeyCode.DownArrow) || joystick.direction.y < 0)
             {
                 //TODO: Bow Down Animation
                 Debug.Log("HOLDDDDDDDDDDDDDDD");
             }
+
+            float velocityRatio = velocity.x / maxVelocity;
+            acceleration = maxAcceleration * (1 - velocityRatio);
+            velocity.x += acceleration * Time.deltaTime;
+
+            if (velocity.x >= maxVelocity)
+            {
+                velocity.x = maxVelocity;
+            }
         }
         else
         {
-            if(Input.GetKeyDown(KeyCode.DownArrow))
-            // if (swipeControlManager.IsSwipingDown())
-            {
-                isFall = true;
-            }
+            // Increase the gravity scale based on the x velocity
+            gravityScale = 1.0f + Mathf.Pow(velocity.x / maxVelocity, 4);
 
-            if (isFall)
+            if (Input.GetKeyDown(KeyCode.DownArrow) || joystick.direction.y < 0)
             {
-                pos.y += gravity * Time.deltaTime;
+                pos.y += gravity * gravityScale * Time.deltaTime;
             }
             else
             {
                 pos.y += velocity.y * Time.deltaTime;
-                velocity.y += gravity * Time.deltaTime;
+
+                velocity.y += gravity * gravityScale * Time.deltaTime;
             }
 
             if (pos.y <= groundHeight)
             {
                 pos.y = groundHeight;
                 isGrounded = true;
-                isFall = false;
             }
         }
 
-
         transform.position = pos;
     }
-
-    // void FixedUpdate()
-    // {
-    //     if (!isGrounded)
-    //     {
-    //         if (isFall)
-    //         {
-    //             pos.y += gravity * Time.fixedDeltaTime;
-    //         }
-    //         else
-    //         {
-    //             pos.y += velocity.y * Time.fixedDeltaTime;
-    //             velocity.y += gravity * Time.fixedDeltaTime;
-    //         }
-    //
-    //
-    //         if (pos.y <= groundHeight)
-    //         {
-    //             pos.y = groundHeight;
-    //             isGrounded = true;
-    //             isFall = false;
-    //         }
-    //     }
-    //
-    //     transform.position = pos;
-    // }
 }
