cba642c46a3f25a5314891ba7e743b7d76f44efb
David Pham <ducptm68@gmail.com>
2023-06-25 23:28:23 +0700

    feat: add tile map

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    //variables visible in the inspector
+    [Header("Spawn settings")] [Space(5)] public float maxSpawnWait;
+    public float minSpawnWait;
+
+    [Header("Add track parts here:")] public GameObject[] trackParts;
+
+    [Header("Add roof parts here:")] public GameObject[] roofParts;
+
+    //not visible in the inspector
+    int lastParcourPart;
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            SpawnNew();
+            float spawnWait = Random.Range(minSpawnWait, maxSpawnWait);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+    
+    void SpawnNew()
+    {
+        if (trackParts.Length > 0)
+        {
+            int random = Random.Range(0, trackParts.Length);
+            if (lastParcourPart != random)
+            {
+                GameObject newParcour =
+                    Instantiate(trackParts[random], transform.position, transform.rotation) as GameObject;
+                newParcour.transform.parent = GameObject.Find("Spawner").transform;
+                lastParcourPart = random;
+            }
+            else
+            {
+                SpawnNew();
+            }
+        }
+    }
+    
+    
+    public void AddRoof()
+    {
+        if (roofParts.Length <= 0) return;
+        
+        //get random roof and instantiate it as a child of the track object
+        int random = Random.Range(0, roofParts.Length);
+        Vector3 pos = gameObject.transform.position;
+
+        pos.x += 17f;
+        
+        GameObject newRoof = Instantiate(roofParts[random], pos, transform.rotation) as GameObject;
+        newRoof.transform.parent = GameObject.Find("Spawner").transform;
+    }
+
+}
