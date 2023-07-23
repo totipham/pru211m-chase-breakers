ff2243c1d486e021e322aadebbe33f0c175a01eb
David Pham <ducptm68@gmail.com>
2023-07-23 23:24:54 +0700

    fix: resolved bullet

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,36 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletController : MonoBehaviour
-{
+public class BulletController : MonoBehaviour {
     private Camera mainCamera;
 
-    void Start()
-    {
+    void Start() {
         mainCamera = Camera.main;
     }
 
-    void Update()
-    {
-        if (!IsVisibleFromCamera())
-        {
+    void Update() {
+        if (!IsVisibleFromCamera()) {
             Destroy(gameObject);
         }
     }
 
-    bool IsVisibleFromCamera()
-    {
+    bool IsVisibleFromCamera() {
         Vector3 viewportPosition =
             mainCamera.WorldToViewportPoint(transform
                 .position);
         return viewportPosition.x >= 0 && viewportPosition.x <= 1 && viewportPosition.y >= 0 &&
                viewportPosition.y <= 1;
     }
-    
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            var _player = GameObject.Find("Player").GetComponent<PlayerController>();
+            var slowDown = _player.SlowDown(true, 0.2f, 0.5f);
+            StartCoroutine(slowDown);
             Destroy(gameObject);
         }
     }
