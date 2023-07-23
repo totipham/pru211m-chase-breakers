594533cf528900dab67e179d6ee533cd455d84a5
David Pham <ducptm68@gmail.com>
2023-07-23 20:41:54 +0700

    feat: add powerup

--- Assets/Scripts/Object/PowerUpItem.cs
+++ Assets/Scripts/Object/PowerUpItem.cs
@@ -4,6 +4,7 @@ namespace Object {
     public class PowerUpItem : Item {
         private void OnCollisionEnter2D(Collision2D other) {
             if (other.gameObject.CompareTag("Player")) {
+                Debug.Log("POWERUP: Player hit powerup");
                 // Drop();
                 // var slowDown = _player.SlowDown(true, slowDownVelocity, effectTime);
                 // StartCoroutine(slowDown);
