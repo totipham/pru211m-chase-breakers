99ff4b5db6d8da331c1f198c405b15451281a769
David Pham <ducptm68@gmail.com>
2023-07-24 00:35:16 +0700

    fix: pause game not work

--- Assets/Scripts/InGameButtonController.cs
+++ Assets/Scripts/InGameButtonController.cs
@@ -7,6 +7,7 @@ public class InGameButtonController : MonoBehaviour
     [SerializeField] private GameObject _pauseButton;
     [SerializeField] private GameObject _resumeButton;
     [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private UIController _uiController; 
 
     public void StartNewGame()
     {
@@ -16,8 +17,8 @@ public class InGameButtonController : MonoBehaviour
 
     public void PauseGame()
     {
-        Debug.Log("Pause game");
         Time.timeScale = 0;
+        _uiController.isPaused = true;
         _resumeButton.SetActive(true);
         _pauseButton.SetActive(false);
         _pauseMenu.SetActive(true);
@@ -26,6 +27,7 @@ public class InGameButtonController : MonoBehaviour
     public void ResumeGame()
     {
         Time.timeScale = 1;
+        _uiController.isPaused = false;
         _resumeButton.SetActive(false);
         _pauseButton.SetActive(true);
         _pauseMenu.SetActive(false);
